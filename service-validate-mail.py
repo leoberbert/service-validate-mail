796ba7379c4a89775bb8f16cc09fdee5e38244b6
Leonardo Berbert <leo4berbert@gmail.com>
2022-02-24 21:38:48 -0300

    Component Adaptation.

--- service-validate-mail.py
+++ service-validate-mail.py
@@ -10,6 +10,7 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask_restful import Resource, Api
 from os import path
 import json,re,sys,os
 from email_validator import validate_email, EmailNotValidError
@@ -20,42 +21,43 @@ if path.exists('blacklist.conf') == False:
 
 config = open("blacklist.conf","r").read().splitlines()
 pattern = '.*|.*'.join(config)
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/api/v1/", methods = ['POST'])
-
-def mailcheck():
-    data = json.loads(request.data)
-    emailAddress = data['emailAddress']
-    blacklist = re.match(pattern,emailAddress,re.IGNORECASE)
-    if blacklist:
-            return jsonify({
-                "emailAddress": str(emailAddress), 
-                "status": 2,
-                "bounce": { 
-                "type": 1, 
-                "detail": "Bad destination mailbox address", 
-                "code": 511
-                }
-            })     
-   
-    if emailAddress:
-        isValidate = ValidateMail(emailAddress)
-        if isValidate == "1": # valid email
-         return jsonify({
-                        "emailAddress": str(emailAddress), 
-                        "status": 1
-                    })            
-        else:
-            return jsonify({
-                "emailAddress": str(emailAddress), 
-                "status": 2,
-                "bounce": { 
-                "type": 1, 
-                "detail": "Invalid mail format", 
-                "code": 990
-                }
-            }) 
+class Mail(Resource):
+    def post(self):
+        data = json.loads(request.data)
+        emailAddress = data['emailAddress']
+        blacklist = re.match(pattern,emailAddress,re.IGNORECASE)
+        if blacklist:
+                return jsonify({
+                    "emailAddress": str(emailAddress), 
+                    "status": 2,
+                    "bounce": { 
+                    "type": 1, 
+                    "detail": "Bad destination mailbox address", 
+                    "code": 511
+                    }
+                })     
+    
+        if emailAddress:
+            isValidate = ValidateMail(emailAddress)
+            if isValidate == "1": # valid email
+                return jsonify({
+                                "emailAddress": str(emailAddress), 
+                                "status": 1
+                            })            
+            else:
+                return jsonify({
+                    "emailAddress": str(emailAddress), 
+                    "status": 2,
+                    "bounce": { 
+                    "type": 1, 
+                    "detail": "Invalid mail format", 
+                    "code": 990
+                    }
+                }) 
 
 def ValidateMail(email):
     try:
@@ -69,5 +71,7 @@ def ValidateMail(email):
     except EmailNotValidError as e:
         print(str(e))
 
+api.add_resource(Mail, '/api/v1/',methods = ['POST']) 
+
 if __name__ == '__main__': 
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
